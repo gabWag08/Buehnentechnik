bf95c1fb416f0b1a0f61b035a82ea618e6108d34
gwagner <gabriel.wagner08@htl-salzburg.ac.at>
2026-03-10 09:24:14 +0100

    Banner und Sound Optimierung

--- Assets/MyGame/Scripts/MuicChanger.cs
+++ Assets/MyGame/Scripts/MuicChanger.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
+
 public class MuicChanger : MonoBehaviour
 {
     public Button button;
@@ -7,20 +9,45 @@ public class MuicChanger : MonoBehaviour
 
     public AudioClip audioClip;
     public AudioClip audioClip2;
+    public VideoClip videoclip;
+    public VideoClip videoclip2;
 
     public AudioSource audioSource;
+    public AudioSource audioSource2;
+    public AudioSource audioSource3;
+    public AudioSource audioSource4;
+    //VideoPlayer
+    public VideoPlayer videoPlayer;
 
     public void ChangeSong1()
     {
-        //Bei Button1 soll in die AudioSource der AudioClip1 geladen und abgespielt werden
         audioSource.clip = audioClip;
         audioSource.Play();
+        audioSource2.clip = audioClip;
+        audioSource2.Play();
+        audioSource3.clip = audioClip;
+        audioSource3.Play();
+        audioSource4.clip = audioClip;
+        audioSource4.Play();
+    
+        videoPlayer.clip = videoclip;
+        videoPlayer.Play();
+    
     }
 
     public void ChangeSong2()
     {
         audioSource.clip = audioClip2;
         audioSource.Play();
+        audioSource2.clip = audioClip2;
+        audioSource2.Play();
+        audioSource3.clip = audioClip2;
+        audioSource3.Play();
+        audioSource4.clip = audioClip2;
+        audioSource4.Play();
+
+        videoPlayer.clip = videoclip2;
+        videoPlayer.Play();
     }
     
 }
--- Assets/MyGame/Scripts/VolumeSlider.cs
+++ Assets/MyGame/Scripts/VolumeSlider.cs
@@ -4,19 +4,31 @@ using UnityEngine.UI;
 public class VolumeSlider : MonoBehaviour
 {
     public AudioSource audioSource;
+    public AudioSource audioSource2;
+    public AudioSource audioSource3;
+    public AudioSource audioSource4;
+
+
     public Slider volumeSlider;
 
     void Start()
     {
         // Anfangswert setzen
         volumeSlider.value = audioSource.volume;
+        volumeSlider.value = audioSource2.volume;
+        volumeSlider.value = audioSource3.volume;
+        volumeSlider.value = audioSource4.volume;
+
 
         // Listener hinzufügen
         volumeSlider.onValueChanged.AddListener(ChangeVolume);
     }
 
-    void ChangeVolume(float value)
+    public void ChangeVolume(float value)
     {
         audioSource.volume = value;
+        audioSource2.volume = value;
+        audioSource3.volume = value;
+        audioSource4.volume = value;
     }
 }
