14da95edc19609b6a6e5f4cf1ca2e14b5131b667
vinz <vinzent.aichinger07@htl-salzburg.ac.at>
2026-04-27 09:16:27 +0200

    New Feature : Numpad for Startscene; not functioanlly integrated yet

---
+++ Assets/MyGame/Scripts/VRNumpad.cs
@@ -0,0 +1,52 @@
+using TMPro;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VRNumpad : MonoBehaviour
+{
+    public TMP_InputField inputField;
+
+    // Input Displays
+    public InputField height;
+    public InputField width;
+    public InputField length;
+
+    public void AddDigit(string digit)
+    {
+        inputField.text += digit;
+    }
+
+    public void Backspace()
+    {
+        if (inputField.text.Length > 0)
+        {
+            inputField.text = inputField.text.Substring(0, inputField.text.Length - 1);
+        }
+    }
+
+    public void Clear()
+    {
+        inputField.text = "";
+    }
+
+    public float GetValue()
+    {
+        float.TryParse(inputField.text, out float value);
+        return value;
+    }
+
+    public void Confirm()
+    {
+        float value = GetValue();
+        Debug.Log("Entered Value: " + value);
+
+    }
+
+    public TMP_InputField currentField;
+
+    public void SetActiveField(TMP_InputField field)
+    {
+        currentField = field;
+    }
+}
